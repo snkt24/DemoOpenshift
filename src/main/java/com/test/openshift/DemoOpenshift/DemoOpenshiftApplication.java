097d27be62c4f338ea7efebaf99a496abe6fee87
Sanket Shah <shahssnkt.ss@gmail.com>
2019-09-27 23:33:41 +0530

    Initial commit

---
+++ src/main/java/com/test/openshift/DemoOpenshift/DemoOpenshiftApplication.java
@@ -0,0 +1,13 @@
+package com.test.openshift.DemoOpenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOpenshiftApplication.class, args);
+	}
+
+}
